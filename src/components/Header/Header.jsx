cb64b386cc265d2c37eda27b56ab78768268845b
Manuel <manuel.lieb@pixelschubser.ch>
2020-04-19 15:56:46 +0200

    update header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 import Logo from '../Logo'
 
+const StyledHeader = styled.header(
+	({ theme: { colors } }) => css`
+		padding: 1em 0;
+		background-color: ${colors.background};
+	`
+)
+
 const Header = () => (
-	<div>
+	<StyledHeader>
 		<Logo />
-		Header
-	</div>
+	</StyledHeader>
 )
 
 export default Header

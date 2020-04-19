24b16b87b378e668cc258b4b8eb436979a9a2f28
Manuel <manuel.lieb@pixelschubser.ch>
2020-04-19 15:56:36 +0200

    add logo svg + styles

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import { em } from 'polished'
+import logo from '../../assets/logo.svg'
 
-const Logo = () => <img src="http://placehold.it/50" alt="Logo" />
+const StyledLogo = styled.img`
+	max-height: ${em(50)};
+	width: auto;
+`
+
+const Logo = () => <StyledLogo src={logo} alt="Logo" />
 
 export default Logo
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,101 @@ import Layout from '../core/helpers/Layout'
 
 export default () => (
 	<Layout>
-		<p>Hello World</p>
+		<p>
+			Suspendisse potenti. Phasellus accumsan cursus velit. Sed magna purus,
+			fermentum eu, tincidunt eu, varius ut, felis. Duis vel nibh at velit
+			scelerisque suscipit. Cras id dui.
+		</p>
+		<p>
+			In consectetuer turpis ut velit. Phasellus magna. Aliquam eu nunc. Aenean
+			massa. Nullam sagittis.
+		</p>
+		<p>
+			Praesent venenatis metus at tortor pulvinar varius. Quisque rutrum. Sed
+			augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Sed
+			magna purus, fermentum eu, tincidunt eu, varius ut, felis. Phasellus nec
+			sem in justo pellentesque facilisis.
+		</p>
+		<p>
+			Pellentesque ut neque. Cras id dui. Quisque libero metus, condimentum nec,
+			tempor a, commodo mollis, magna. Etiam rhoncus. Integer ante arcu,
+			accumsan a, consectetuer eget, posuere ut, mauris.
+		</p>
+		<p>
+			Quisque malesuada placerat nisl. Praesent egestas tristique nibh. Morbi ac
+			felis. Etiam iaculis nunc ac metus. Quisque ut nisi.
+		</p>
+		<p>
+			Suspendisse potenti. Phasellus accumsan cursus velit. Sed magna purus,
+			fermentum eu, tincidunt eu, varius ut, felis. Duis vel nibh at velit
+			scelerisque suscipit. Cras id dui.
+		</p>
+		<p>
+			In consectetuer turpis ut velit. Phasellus magna. Aliquam eu nunc. Aenean
+			massa. Nullam sagittis.
+		</p>
+		<p>
+			Praesent venenatis metus at tortor pulvinar varius. Quisque rutrum. Sed
+			augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Sed
+			magna purus, fermentum eu, tincidunt eu, varius ut, felis. Phasellus nec
+			sem in justo pellentesque facilisis.
+		</p>
+		<p>
+			Pellentesque ut neque. Cras id dui. Quisque libero metus, condimentum nec,
+			tempor a, commodo mollis, magna. Etiam rhoncus. Integer ante arcu,
+			accumsan a, consectetuer eget, posuere ut, mauris.
+		</p>
+		<p>
+			Quisque malesuada placerat nisl. Praesent egestas tristique nibh. Morbi ac
+			felis. Etiam iaculis nunc ac metus. Quisque ut nisi.
+		</p>
+		<p>
+			Suspendisse potenti. Phasellus accumsan cursus velit. Sed magna purus,
+			fermentum eu, tincidunt eu, varius ut, felis. Duis vel nibh at velit
+			scelerisque suscipit. Cras id dui.
+		</p>
+		<p>
+			In consectetuer turpis ut velit. Phasellus magna. Aliquam eu nunc. Aenean
+			massa. Nullam sagittis.
+		</p>
+		<p>
+			Praesent venenatis metus at tortor pulvinar varius. Quisque rutrum. Sed
+			augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Sed
+			magna purus, fermentum eu, tincidunt eu, varius ut, felis. Phasellus nec
+			sem in justo pellentesque facilisis.
+		</p>
+		<p>
+			Pellentesque ut neque. Cras id dui. Quisque libero metus, condimentum nec,
+			tempor a, commodo mollis, magna. Etiam rhoncus. Integer ante arcu,
+			accumsan a, consectetuer eget, posuere ut, mauris.
+		</p>
+		<p>
+			Quisque malesuada placerat nisl. Praesent egestas tristique nibh. Morbi ac
+			felis. Etiam iaculis nunc ac metus. Quisque ut nisi.
+		</p>
+		<p>
+			Suspendisse potenti. Phasellus accumsan cursus velit. Sed magna purus,
+			fermentum eu, tincidunt eu, varius ut, felis. Duis vel nibh at velit
+			scelerisque suscipit. Cras id dui.
+		</p>
+		<p>
+			In consectetuer turpis ut velit. Phasellus magna. Aliquam eu nunc. Aenean
+			massa. Nullam sagittis.
+		</p>
+		<p>
+			Praesent venenatis metus at tortor pulvinar varius. Quisque rutrum. Sed
+			augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Sed
+			magna purus, fermentum eu, tincidunt eu, varius ut, felis. Phasellus nec
+			sem in justo pellentesque facilisis.
+		</p>
+		<p>
+			Pellentesque ut neque. Cras id dui. Quisque libero metus, condimentum nec,
+			tempor a, commodo mollis, magna. Etiam rhoncus. Integer ante arcu,
+			accumsan a, consectetuer eget, posuere ut, mauris.
+		</p>
+		<p>
+			Quisque malesuada placerat nisl. Praesent egestas tristique nibh. Morbi ac
+			felis. Etiam iaculis nunc ac metus. Quisque ut nisi.
+		</p>
 	</Layout>
 )

32492608b2fe893722741a0f6cd04fd87551c45f
Manuel <manuel.lieb@pixelschubser.ch>
2020-04-23 10:05:40 +0200

    add Footer Copyright

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,33 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 
-const Footer = () => <div>Footer</div>
+const StyledFooter = styled.footer(
+	({ theme: { colors } }) => css`
+		padding: 1em 0;
+
+		hr {
+			border-color: ${colors.comment};
+		}
+	`
+)
+
+const StyledCopyright = styled.small(
+	({ theme: { colors } }) => css`
+		color: ${colors.comment};
+	`
+)
+
+const Footer = () => {
+	const year = new Date().getFullYear()
+
+	return (
+		<StyledFooter>
+			<p>Footer</p>
+			<hr />
+			<StyledCopyright>&copy;{year} - Made with &#9829; by Manuel Lieb</StyledCopyright>
+		</StyledFooter>
+	)
+}
 
 export default Footer

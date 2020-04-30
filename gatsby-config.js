c277de83aa9bc1d28ad90a554e3d990304728e96
Manuel <manuel.lieb@pixelschubser.ch>
2020-04-30 10:46:39 +0200

    configure prismic source

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
 	/* Your site config here */
 	plugins: [
@@ -18,6 +22,23 @@ module.exports = {
 						weights: ['300..700'],
 					},
 				],
+			},
+		},
+		{
+			resolve: 'gatsby-source-prismic-graphql',
+			options: {
+				repositoryName: 'pixelschubser',
+				accessToken: `${process.env.API_KEY}`,
+				path: '/preview',
+				previews: true,
+				// pages: [
+				// 	{
+				// 		type: 'Article', // TypeName from prismic
+				// 		match: '/article/:uid', // Pages will be generated under this pattern
+				// 		path: '/article', // Placeholder page for unpublished documents
+				// 		component: require.resolve('./src/templates/article.js'),
+				// 	},
+				// ],
 			},
 		},
 	],

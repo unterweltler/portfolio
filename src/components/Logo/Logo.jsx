2a42f5f11c75fc9fbac09fc234b2eb13bb9d3761
Manuel <manuel.lieb@pixelschubser.ch>
2020-05-01 15:42:37 +0200

    add hover effect to logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,19 +2,61 @@ import React from 'react'
 import styled from '@emotion/styled'
 import { css } from '@emotion/core'
 import { Link } from 'gatsby'
-import logo from '../../assets/logo.svg'
+import { useTheme } from 'emotion-theming'
+import { SvgLogo } from '../../assets'
 
-const StyledLogo = styled.img(
-	({ theme: { mixins } }) => css`
-		width: auto;
-		${mixins.fluidStyle('max-height', { min: 32, max: 48 })};
+const logoHeight = { min: 32, max: 48 }
+
+const StyledLink = styled(Link)(
+	({
+		theme: {
+			animations: { duration, timingFunc },
+			mixins,
+		},
+	}) => css`
+		display: inline-block;
+		${mixins.fluidStyle('height', logoHeight)};
+
+		svg {
+			transition: transform ${duration}s ${timingFunc};
+		}
+
+		&:hover {
+			svg {
+				&:nth-of-type(2) {
+					transform: translate3d(2px, 2px, 0);
+					transition-delay: 0.1s;
+				}
+
+				&:nth-of-type(3) {
+					transform: translate3d(2px, -2px, 0);
+					transition-delay: 0.2s;
+				}
+			}
+		}
 	`
 )
 
-const Logo = () => (
-	<Link to="/">
-		<StyledLogo src={logo} alt="Logo" />
-	</Link>
+const StyledLogo = styled(SvgLogo, {
+	shouldForwardProp: prop => prop !== 'options',
+})(
+	({ theme: { mixins }, options: { zIndex } }) => css`
+		position: absolute;
+		${mixins.fluidStyle('height', logoHeight)};
+		z-index: ${zIndex};
+	`
 )
+
+const Logo = () => {
+	const theme = useTheme()
+
+	return (
+		<StyledLink to="/">
+			<StyledLogo color={theme.colors.foreground} options={{ zIndex: 3 }} />
+			<StyledLogo color={theme.colors.pink} options={{ zIndex: 2 }} />
+			<StyledLogo color={theme.colors.cyan} options={{ zIndex: 1 }} />
+		</StyledLink>
+	)
+}
 
 export default Logo

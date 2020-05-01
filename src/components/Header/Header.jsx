eeae289178c47bb69d60dfff824ee1aa100a611f
Manuel <manuel.lieb@pixelschubser.ch>
2020-05-01 15:43:12 +0200

    increase header vertical padding

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { css } from '@emotion/core'
+import { rem } from 'polished'
 import Logo from '../Logo'
 
 const StyledHeader = styled.header(
 	({ theme: { colors } }) => css`
 		grid-column: 2 / 5;
-		padding: 1em 0;
+		padding: ${rem(24)} 0;
 		background-color: ${colors.background};
 	`
 )

cff5392a95a00ad7b2ed8d1e043865629359228b
Manuel <manuel.lieb@pixelschubser.ch>
2020-05-01 15:19:38 +0200

    add SVGR and replace Logo

---
+++ .svgr/index.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+function defaultIndexTemplate(filePaths) {
+	const exportEntries = filePaths.map(filePath => {
+		const basename = path.basename(filePath, path.extname(filePath))
+		return `export { default as Svg${basename} } from './${basename}'`
+	})
+	return exportEntries.join('\n')
+}
+module.exports = defaultIndexTemplate

52a8bdf655384f931bbaca723e4c612428e98b67
Manuel <manuel.lieb@pixelschubser.ch>
2020-06-07 09:52:09 +0200

    add projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,3 +1,42 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../core/helpers/Layout'
+import ProjectList from '../components/ProjectList'
+import LinkBlock from '../components/LinkBlock'
 
-export default () => <div>Projects Overview Page</div>
+export default ({ data }) => {
+	const projects = data.prismic.allProjects.edges
+
+	return (
+		<Layout title="Project List">
+			<LinkBlock back={{ to: '/', text: 'Back' }} />
+			<ProjectList title="Project List" projects={projects} showBackLink />
+		</Layout>
+	)
+}
+
+export const query = graphql`
+	{
+		prismic {
+			allProjects(sortBy: date_DESC) {
+				edges {
+					node {
+						title
+						image
+						imageSharp {
+							childImageSharp {
+								fluid(traceSVG: { background: "#f8f8f2", color: "#282a36" }) {
+									...GatsbyImageSharpFluid_withWebp_tracedSVG
+								}
+							}
+						}
+						_meta {
+							uid
+							type
+						}
+					}
+				}
+			}
+		}
+	}
+`
